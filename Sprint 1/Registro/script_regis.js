f409437a76fff73db0d0ad7022314651ad3847a3
Camilaux <mzapataar@unal.edu.co>
2024-02-25 12:07:08 -0500

    cambios al css y js

--- Sprint 1/Registro/script_regis.js
+++ Sprint 1/Registro/script_regis.js
@@ -7,10 +7,15 @@ document.addEventListener('DOMContentLoaded', function() {
     
     // Obtén la caja trasera de registro por su clase
     var cajaTraseraRegistro = document.querySelector('.caja_trasera-registro');
+
+    var contenedorlore = document.querySelector(".contenedor_login-registro");
+
+    var casatrasera = document.querySelector(".caja_trasera")
     
     // Define la función que se ejecutará cuando se haga clic en el botón de registro
     function mostrarRegistro() {
         // Verifica si los elementos existen para evitar errores
+       
         if(formularioRegistro && cajaTraseraRegistro) {
             formularioRegistro.style.display = 'block'; // Muestra el formulario de registro
             cajaTraseraRegistro.style.display = 'none'; // Oculta la caja trasera
